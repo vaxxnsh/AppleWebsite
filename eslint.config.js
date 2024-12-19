06b237ac23a218b53df17d42f3e2ee31902625e6
vaxxnsh <kakshit817@gmail.com>
2024-12-19 17:55:55 +0530

    0.1 Till FeatureContainer

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactThree from '@react-three/eslint-plugin'
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +23,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@react-three': reactThree,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -33,6 +35,8 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      '@react-three/no-outside-canvas': 'warn',
+      '@react-three/no-invalid-geometry-attributes': 'error',
     },
   },
 ]
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Navbar from "./components/Navbar"
+import Model from "./components/Model"
+import Features from "./components/Features"
+import HowItWorks from "./components/HowItWorks"
 
 function App() {
   
@@ -10,6 +13,9 @@ function App() {
       <Navbar/>
       <Hero/>
       <Highlights/>
+      <Model/>
+      <Features/>
+      <HowItWorks/>
     </main>
   )
 }
